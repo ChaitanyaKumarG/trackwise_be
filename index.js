2730576d549837885c2687368f80ddcb7e0ee57b
G Chaithanya Kumar <20551a0220.chaitanya@gmail.com>
2025-01-07 01:09:48 +0530

    urls

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 
 //Middlewares
 app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(bodyParser.json());
